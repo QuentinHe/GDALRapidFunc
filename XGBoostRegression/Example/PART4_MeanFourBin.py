cc4469db825ce6b23a1b9629aea94d27e85e5cea
QuentinHe <mix492248932@gmail.com>
2023-10-07 14:40:51 +0800

    对4个不同Bin结果按照每个像元计算一次均值，以抵消不同Bin结果的影响的实例。

---
+++ XGBoostRegression/Example/PART4_MeanFourBin.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/mgdal_env
+# @Time : 2023/10/7 11:23
+# @Author : Hexk
+# @Descript : 对每年的4个Bin等级对应点进行平均
+import os
+import XGBoostRegression.IntegrationXGBoostRegression as IXGBR
+
+os.environ['PROJ_LIB'] = 'D:/Mambaforge/envs/mgdal_env/Library/share/proj'
+os.environ['GDAL_DATA'] = 'D:/Mambaforge/envs/mgdal_env/Library/share'
+
+if __name__ == '__main__':
+    raster_folder = r'E:\Glacier_DEM_Register\Tanggula_FourYear_Data\Test_20231004\1_PredictData\3_MaskData'
+    output_folder = r'E:\Glacier_DEM_Register\Tanggula_FourYear_Data\Test_20231004\1_PredictData\4_MeanData'
+    IXGBR.MeanBinsRaster(raster_folder, _output_path=output_folder)
