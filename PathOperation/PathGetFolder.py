40c543b5b1ad35c2f69197974d9431d43faed2d6
QuentinHe <mix492248932@gmail.com>
2023-10-12 10:59:33 +0800

    新创建了一个获取当前文件夹下，该层级的所有文件夹

---
+++ PathOperation/PathGetFolder.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/mgdal_env
+# @Time : 2023/10/12 9:27
+# @Author : Hexk
+# @Descript : 获取指定folder下的所有folder，不包含子folder
+
+import os
+
+
+def PathGetFolders(_path_folder):
+    """
+    获取指定folder下的所有folder，不包含子folder
+    :param _path_folder: 文件夹路径
+    :return: 绝对文件夹路径list，文件夹名称list，不包含下一级子文件夹
+    """
+    _name_list = os.listdir(_path_folder)
+    _folder_path_list = []
+    _folder_name_list = []
+    for index, item in enumerate(_name_list):
+        # 这里不能采用remove移除，会导致指针错误遗漏一个元素
+        if os.path.isdir(os.path.join(_path_folder, item)):
+            _folder_name_list.append(item)
+            _folder_path_list.append(os.path.join(_path_folder, item))
+        else:
+            continue
+    return _folder_path_list, _name_list
+
+
+if __name__ == '__main__':
+    path_folder = r'E:\Glacier_DEM_Register\Tanggula_FourYear_Data\Test_20231009\0_BasePoint\NASA_2019_Bin_50'
+    # path_folder = r'E:\Glacier_DEM_Register\Tanggula_FourYear_Data\Test_20231009\0_BasePoint'
+    folder_path_list, folder_name_list = PathGetFolders(path_folder)
